b332cf1f2f94a025069ad4f49955dbf6d9708e0e
Rajal Srivastava <rajal10srivastava@gmail.com>
2025-03-07 12:29:51 +0530

    implement delete form functionality

--- components/FormList.tsx
+++ components/FormList.tsx
@@ -1,14 +1,27 @@
+"use client"
 import Link from "next/link"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
 import { Button } from "./ui/button"
 import { Edit2 } from "lucide-react"
 import { Form } from "@/types/form"
+import { deleteForm } from "@/actions/deleteForm"
+import toast from "react-hot-toast"
 
 type Props = {
     form:Form
 }
 const FormList : React.FC<Props> = ({form}) => {
-    console.log(form);
+
+  const deleteFormHandler =async (formId:number)=>{
+    const data = await deleteForm(formId);
+
+    if(data.success){
+      toast.success(data.message);
+    }else{
+      toast.error(data.message);
+    }
+    }
+  
 
     return (
       <div>
@@ -25,11 +38,11 @@ const FormList : React.FC<Props> = ({form}) => {
               <Button variant={'link'} className="text-blue-600">Submissions - {form.submission}</Button>
             </Link>
           </CardContent>
-          <CardFooter>
+          <CardFooter className="flex justify-between">
             <Button variant="outline">
               <Edit2 /> Edit
             </Button>
-            <Button variant="destructive">Delete</Button>
+            <Button onClick={()=>deleteFormHandler(form.id)} variant="destructive">Delete</Button>
           </CardFooter>
         </Card>
       </div>
